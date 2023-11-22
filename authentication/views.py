eee41990fbacfce8cea24f6e4868fb591fcf5613
divieurora <divieurora@gmail.com>
2023-11-22 11:28:54 +0700

    add bonus tugas 9

--- authentication/views.py
+++ authentication/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth import authenticate, login as auth_login, logout as auth_logout
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.forms import UserCreationForm
 
 @csrf_exempt
 def login(request):
@@ -46,3 +47,23 @@ def logout(request):
         "status": False,
         "message": "Logout gagal."
         }, status=401)
+
+@csrf_exempt
+def register(request):    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return JsonResponse({
+                "status": True,
+                "message": "Berhasil mendaftar akun baru!"
+            }, status=200)
+        else:
+            return JsonResponse({
+                "status": False,
+                "message": "Maaf, username atau password tidak valid. Silahkan coba lagi."
+            }, status=401)
+    return JsonResponse({
+        "status": False,
+        "message": "Method not allowed."
+    }, status=405)
